4c1eb82f883efbc9ad362671b901097400927a20
Sarah Gemperle <sgemperl@ucsd.edu>
2017-02-03 23:05:42 -0800

    Initial Commit

---
+++ TwitterDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TwitterDemo
+//
+//  Created by Sarah Gemperle on 2/3/17.
+//  Copyright © 2017 Sarah Gemperle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
